30a73e164bf66181d77abf77bbad0a402a7fbcd2
Aanupriya2206 <108046953+Aanupriya2206@users.noreply.github.com>
2023-04-06 19:37:29 +0530

    commit message

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logo'>
+        <h1>NETFLIX</h1>
+      </div>
+      <div className='btn'>
+        <button type="login">Sign In</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className='col'>
+        <h1>See what's next</h1>
+        <h3>WATCH ANYTIME. CANCEL ANYTIME</h3>
+        <div className='form'>
+          <button className='btn'>WATCH FREE FOR 30 DAYS</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
